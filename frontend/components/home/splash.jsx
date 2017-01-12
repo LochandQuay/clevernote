4be17d6bcd16d7234ebc407446479e002c08d105
Hope <hopewanroy@gmail.com>
2017-01-12 13:21:35 -0800

    add text overlay on splash

--- frontend/components/home/splash.jsx
+++ frontend/components/home/splash.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+// import QuickAuthContainer from './quick_auth_container';
 
 const Splash = () => (
   <div className="home-main=content">
     <img
       src="http://res.cloudinary.com/clevernote/image/upload/q_100/v1484173061/splash.jpg"
       className="splash-image" />
+    <div className="splash-image-text-overlay">
+      <h1>clevernote</h1>
+    </div>
+
   </div>
 );
 
 export default Splash;
+
+// <QuickAuthContainer />
