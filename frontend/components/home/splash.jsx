c5e52ef4a255d1d41a1ff7a9fbbf429a1f3b7d12
Hope <hopewanroy@gmail.com>
2017-01-12 23:47:16 -0800

    change image links to https

--- frontend/components/home/splash.jsx
+++ frontend/components/home/splash.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Splash = () => (
   <div className="home-main=content">
     <img
-      src="http://res.cloudinary.com/clevernote/image/upload/q_100/v1484173061/splash.jpg"
+      src="https://res.cloudinary.com/clevernote/image/upload/v1484173061/splash.jpg"
       className="splash-image" />
     <div className="splash-image-text-overlay">
       <h1>clevernote</h1>
