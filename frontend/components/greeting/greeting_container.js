a0e54ba030ac06fada6bf3e30fa90edb6c8016cf
Hope <hopewanroy@gmail.com>
2017-09-21 16:10:36 -0700

    appease linter

--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -3,16 +3,15 @@ import { login, logout, clearErrors } from '../../actions/session_actions';
 import Header from './greeting';
 
 const mapStateToProps = ({ session }) => ({
-  currentUser: session.currentUser
+  currentUser: session.currentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logout()),
   login: user => dispatch(login(user)),
-  clearErrors: () => dispatch(clearErrors())
+  clearErrors: () => dispatch(clearErrors()),
 });
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
-)(Header);
+  mapDispatchToProps)(Header);
