352891ca26ca42af8afbd6b7e248c086c798ee32
Hope <hopewanroy@gmail.com>
2017-09-21 22:11:54 -0700

    add PropType validation to notebook index

--- frontend/components/notebooks/notebook_index_container.js
+++ frontend/components/notebooks/notebook_index_container.js
@@ -1,24 +1,21 @@
 import { connect } from 'react-redux';
 import NotebookIndex from './notebook_index';
-import { fetchNotebooks, createNotebook, setCurrentNotebook }
+import { createNotebook, setCurrentNotebook }
   from '../../actions/notebook_actions';
-
 import { alphaSort } from '../../reducers/selectors';
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   currentUser: state.session.currentUser,
   currentNotebook: state.notebooks.byId[state.notebooks.currentNotebook],
-  notebooks: alphaSort(state.notebooks)
+  notebooks: alphaSort(state.notebooks),
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  // fetchNotebooks: () => dispatch(fetchNotebooks()),
-  createNotebook: notebook => ownProps.createNotebook(notebook),
   closeNotebooksModal: ownProps.closeNotebooksModal,
-  setCurrentNotebook: notebook => dispatch(setCurrentNotebook(notebook))
+  createNotebook: notebook => dispatch(createNotebook(notebook)),
+  setCurrentNotebook: notebook => dispatch(setCurrentNotebook(notebook)),
 });
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
-)(NotebookIndex);
+  mapDispatchToProps)(NotebookIndex);
