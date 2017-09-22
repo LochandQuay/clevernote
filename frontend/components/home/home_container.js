9ad3fbef149328cfd466796546318875790d7a30
Hope <hopewanroy@gmail.com>
2017-09-21 20:20:55 -0700

    remove dead code and refactor for airbnb style

--- frontend/components/home/home_container.js
+++ frontend/components/home/home_container.js
@@ -1,39 +1,22 @@
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import Home from './home';
-// import { fetchNotes, setCurrentNote, createNote }
-//   from '../../actions/note_actions';
-// import { fetchNotebooks, setCurrentNotebook }
-//   from '../../actions/notebook_actions';
 import { fetchTags }
   from '../../actions/tag_actions';
-import { logout } from '../../actions/session_actions';
-
 import { fetchNotes } from '../../actions/note_actions';
 import { fetchNotebooks } from '../../actions/notebook_actions';
 
-const mapStateToProps = ({session, notes, notebooks, tags}) => {
-  return ({
-    currentUser: session.currentUser,
-    // currentNote: notes.currentNote,
-    // currentNotebook: notebooks.currentNotebook,
-    // notes: notes.notes,
-    // tags: tags.sortedTags,
-    // errors: session.errors
-    loggedIn: Boolean(session.currentUser)
-  });
-};
+const mapStateToProps = ({ session }) => ({
+  currentUser: session.currentUser,
+  loggedIn: Boolean(session.currentUser),
+});
 
 const mapDispatchToProps = dispatch => ({
   fetchNotes: () => dispatch(fetchNotes()),
-  // setCurrentNote: (note) => dispatch(setCurrentNote(note)),
   fetchNotebooks: () => dispatch(fetchNotebooks()),
-  // setCurrentNotebook: (notebook) => dispatch(setCurrentNotebook(notebook)),
-  // createNote: (note) => dispatch(createNote(note))
   fetchTags: () => dispatch(fetchTags()),
 });
 
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
-)(Home);
+  mapDispatchToProps)(Home);
