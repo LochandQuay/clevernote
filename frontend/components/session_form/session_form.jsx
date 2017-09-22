f3572048ee0d6a5b7092d68b532ae6dc377af94f
Hope <hopewanroy@gmail.com>
2017-09-21 22:51:58 -0700

    add PropType validations for session form and sidebar

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
 
 class SessionForm extends React.Component {
   constructor(props) {
@@ -7,18 +8,22 @@ class SessionForm extends React.Component {
 
     this.state = {
       formType: this.props.formType,
-      username: "", password: "", name: "", email: ""};
+      username: '',
+      password: '',
+      name: '',
+      email: '',
+    };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.toggleFormType = this.toggleFormType.bind(this);
     this.displaySignUpField = this.displaySignUpField.bind(this);
   }
 
-  componentWillMount() {
-    if (this.props.demoUser) {
-      this.props.processForm(this.props.demoUser);
-    }
-  }
+  // componentWillMount() {
+  //   if (this.props.demoUser) {
+  //     this.props.processForm(this.props.demoUser);
+  //   }
+  // }
 
   componentDidUpdate() {
     this.redirectIfLoggedIn();
@@ -34,35 +39,21 @@ class SessionForm extends React.Component {
     e.preventDefault();
     if (this.state.formType === 'signup') {
       this.props.signup(this.state);
-    }
-    else {
+    } else {
       this.props.login(this.state);
     }
   }
 
   update(field) {
-    return e => this.setState({[field]: e.target.value});
+    return e => this.setState({ [field]: e.target.value });
   }
-
-  renderErrors() {
-		return(
-			<ul>
-				{this.props.errors.map((error, i) => (
-					<li key={`error-${i}`}>
-						{error}
-					</li>
-				))}
-			</ul>
-		);
-	}
 
   toggleFormType() {
     this.props.clearErrors();
     if (this.state.formType === 'signup') {
-      this.setState({formType: 'login'});
-    }
-    else {
-      this.setState({formType: 'signup'});
+      this.setState({ formType: 'login' });
+    } else {
+      this.setState({ formType: 'signup' });
     }
   }
 
@@ -70,72 +61,104 @@ class SessionForm extends React.Component {
     if (this.state.formType === 'signup') {
       return (
         <div>
-          <label>
+          <label htmlFor="Name">
             <input
               type="text"
               placeholder="Name"
               value={this.state.name}
-              onChange={this.update('name')} />
+              onChange={this.update('name')}
+            />
           </label>
 
           <br /><br />
 
-            <label>
-              <input
-                type="text"
-                placeholder="Email Address"
-                value={this.state.email}
-                onChange={this.update('email')} />
-            </label>
+          <label htmlFor="Email Address">
+            <input
+              type="text"
+              placeholder="Email Address"
+              value={this.state.email}
+              onChange={this.update('email')}
+            />
+          </label>
 
-            <br /><br />
+          <br /><br />
         </div>
       );
     }
-    else {
-      return (<div></div>);
+    return (<div />);
+  }
+
+  renderErrors() {
+    return (
+      <ul>
+        {this.props.errors.map(error => (
+          <li key={`error-${error}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  renderFormTypeText() {
+    if (this.state.formType === 'login') {
+      return (
+        <div
+          className="toggle-form-link"
+          role="button"
+          tabIndex={0}
+          onClick={this.toggleFormType}
+        >
+          <h4>Don&apos;t have an account?</h4>
+          <h3>Sign Up!</h3>
+        </div>
+      );
     }
+    return (
+      <div
+        className="toggle-form-link"
+        role="button"
+        tabIndex={0}
+        onClick={this.toggleFormType}
+      >
+        <h4>Already have an account?</h4>
+        <h3>Log in!</h3>
+      </div>
+    );
   }
 
   render() {
     const sessionText =
-      (this.state.formType === 'login') ? "Log In" : "Sign Up";
-
-    const toggleFormTypeText =
-      (this.state.formType === 'login') ?
-      (<div className="toggle-form-link" onClick={this.toggleFormType}>
-        <h4>Don't have an account?</h4><h3>Sign Up!</h3>
-      </div>) :
-      (<div className="toggle-form-link" onClick={this.toggleFormType}>
-        <h4>Already have an account?</h4><h3>Log in!</h3>
-      </div>);
+      (this.state.formType === 'login') ? 'Log In' : 'Sign Up';
 
     return (
-      <div className = "auth-form">
+      <div className="auth-form">
         <h2>{sessionText}</h2>
 
         <form onSubmit={this.handleSubmit}>
 
           {this.displaySignUpField()}
 
-          <label>
+          <label htmlFor="Username">
             <input
               type="text"
               placeholder="Username"
               value={this.state.username}
               onChange={this.update('username')}
-              onClick={this.props.clearErrors} />
+              onClick={this.props.clearErrors}
+            />
           </label>
 
           <br /><br />
 
-          <label>
+          <label htmlFor="Password">
             <input
               type="password"
               placeholder="Password"
               value={this.state.password}
               onChange={this.update('password')}
-              onClick={this.props.clearErrors} />
+              onClick={this.props.clearErrors}
+            />
           </label>
 
           <br /><br />
@@ -143,17 +166,46 @@ class SessionForm extends React.Component {
           <input
             type="submit"
             value={sessionText}
-            className="submit-button" />
+            className="submit-button"
+          />
         </form>
 
         <br />
-        { this.renderErrors() }
+        {this.renderErrors()}
         <br /><br />
-          {toggleFormTypeText}
+        {this.renderFormTypeText()}
       </div>
     );
   }
-
 }
+
+SessionForm.propTypes = {
+  formType: PropTypes.string.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
+  signup: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired,
+  clearErrors: PropTypes.func.isRequired,
+  errors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  router: PropTypes.shape({
+    createHref: PropTypes.func,
+    createKey: PropTypes.func,
+    createLocation: PropTypes.func,
+    createPath: PropTypes.func,
+    getCurrentLocation: PropTypes.func,
+    go: PropTypes.func,
+    goBack: PropTypes.func,
+    goForward: PropTypes.func,
+    isActive: PropTypes.func,
+    listen: PropTypes.func,
+    listenBefore: PropTypes.func,
+    location: PropTypes.object,
+    params: PropTypes.object,
+    push: PropTypes.func,
+    replace: PropTypes.func,
+    routes: PropTypes.arrayOf(PropTypes.object),
+    setRouteLeaveHook: PropTypes.func,
+    transitionTo: PropTypes.func,
+  }).isRequired,
+};
 
 export default withRouter(SessionForm);
